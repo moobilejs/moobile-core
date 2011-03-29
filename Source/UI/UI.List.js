6932f3a16cd72432b30eced35f5342595bc2751f
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-03-28 22:26:40 -0400

    Added a the UI.List control.

--- Source/UI/UI.List.js
+++ Source/UI/UI.List.js
@@ -1,6 +1,141 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+---
 
+name: UI.List
 
+description: Provide a list of items.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- UI.Control
+	- UI.ListItem
+
+provides:
+	- UI.List
+
+...
+*/
+
+UI.List = new Class({
+
+	Extends: UI.Control,
+
+	items: [],
+
+	selectedItems: [],
+
+	options: {
+		className: 'ui-list',
+		selectable: true,
+		multiple: false
+	},
+
+	setup: function() {
+		this.attachItems();
+		return this.parent();
+	},
+
+	destroy: function() {
+		this.destroyItems();
+		return this.parent();
+	},
+
+	destroyItems: function() {
+		this.items.each(function(item) { item.destroy(); });
+		this.items = null;
+		this.items = [];
+		return this;
+	},
+
+	attachItems: function() {
+		this.element.getElements('[data-role=list-item]').each(this.attachItem.bind(this));
+		return this;
+	},
+
+	attachItem: function(element) {
+		var item = new UI.ListItem(element);
+		item.setSelectable(this.options.selectable);
+		item.addEvent(Event.SELECT, this.bound('onSelect'));
+		item.addEvent(Event.DESELECT, this.bound('onDeselect'));
+		this.items.push(item);
+		return this;
+	},
+
+	detachItems: function() {
+		this.item = null;
+		this.item = [];
+		return this;
+	},
+
+	detachItem:function(item) {
+		this.item.remove(item);
+		return this;
+	},
+
+	setSelectable: function(selectable) {
+		this.options.selectable = selectable;
+		this.items.each(function(item) { item.setSelectable(selectable) });
+		return this;
+	},
+
+	setSelectedItem: function(item) {
+		this.setItemAsSelected(item.setSelected(true));
+		return this;
+	},
+
+	setSelectedItems: function() {
+		Array.each(arguments, function(item) { this.setSelectedItem(item) }.bind(this));
+		return this;
+	},
+
+	removeSelectedItem: function(item) {
+		this.setItemAsDeselected(item.setSelected(false));
+	},
+
+	removeSelectedItems: function() {
+		Array.each(arguments, function(item) { this.removeSelectedItem(item) }.bind(this));
+		return this;
+	},
+
+	clearSelectedItems: function() {
+		this.removeSelectedItems.apply(this, this.selectedItems);
+	},
+
+	setItemAsSelected: function(item) {
+		if (this.options.multiple == false) {
+			this.selectedItems.each(function(item) { item.setSelected(false); });
+			this.selectedItems = null;
+			this.selectedItems = []
+		}
+		this.selectedItems.push(item);
+		this.fireEvent(Event.SELECT, item);
+		return this;
+	},
+
+	setItemAsDeselected: function(item) {
+		this.selectedItems.remove(item);
+		this.fireEvent(Event.DESELECT, item);
+		return this;
+	},
+
+	getSelectedItem: function() {
+		return this.selectedItems.getLast();
+	},
+
+	getSelectedItems: function() {
+		return this.selectedItems;
+	},
+
+	onSelect: function(item) {
+		return this.setItemAsSelected(item);
+	},
+
+	onDeselect: function(item) {
+		return this.setItemAsDeselected(item);
+	}
+
+});
--- Source/UI/UI.ListItem.js
+++ Source/UI/UI.ListItem.js
@@ -1,6 +1,125 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+---
 
+name: UI.ListItem
 
+description: Provide an item of a list.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- UI.Control
+
+provides:
+	- UI.ListItem
+
+...
+*/
+
+UI.ListItem = new Class({
+
+	Extends: UI.Control,
+
+	wrapper: null,
+
+	selected: false,
+
+	options: {
+		className: 'ui-list-item',
+		selectable: true
+	},
+
+	initialize: function(element, options) {
+		this.parent(element, options);
+		this.selectable = this.options.selectable;
+		return this;
+	},
+
+	setup: function() {
+		this.injectWrapper();
+		return this.parent();
+	},
+
+	destroy: function() {
+		this.injectWrapper();
+		return this.parent();
+	},
+
+	injectWrapper: function() {
+		this.wrapper = new Element('div.' + this.options.className + '-wrapper').set('html', this.element.get('html'));
+		this.element.empty();
+		this.element.adopt(this.wrapper);
+		return this;
+	},
+
+	destroyWrapper: function() {
+		this.wrapper.destroy();
+		this.wrapper = null;
+		return this;
+	},
+
+	attachEvents: function() {
+		this.element.addEvent(Event.CLICK, this.bound('onClick'));
+		this.element.addEvent(Event.MOUSE_UP, this.bound('onMouseUp'))
+		this.element.addEvent(Event.MOUSE_DOWN, this.bound('onMouseDown'));
+		return this.parent();
+	},
+
+	detachEvents: function() {
+		this.element.removeEvent(Event.CLICK, this.bound('onClick'));
+		this.element.removeEvent(Event.MOUSE_UP, this.bound('onMouseUp'));
+		this.element.removeEvent(Event.MOUSE_DOWN, this.bound('onMouseDown'));
+		return this.parent();
+	},
+
+	setSelectable: function(selectable) {
+		this.options.selectable = selectable;
+	},
+
+	toggleSelected: function() {
+		return this.setSelected(!this.selected);
+	},
+
+	setSelected: function(selected) {
+		if (this.selected != selected) {
+			this.selected = selected;
+			if (this.selected) {
+				this.addClass(this.options.className + '-selected');
+				this.fireEvent(Event.SELECT, this);
+			} else {
+				this.removeClass(this.options.className + '-selected');
+				this.fireEvent(Event.DESELECT, this);
+			}			
+		}
+		return this;
+	},
+
+	isSelected: function() {
+		return this.selected;
+	},
+
+	onClick: function(e) {
+		e.target = this;
+		this.fireEvent(Event.CLICK, e);
+		if (this.options.selectable) this.toggleSelected();
+		return this;
+	},
+
+	onMouseDown: function(e) {
+		e.target = this;
+		this.fireEvent(Event.MOUSE_DOWN, e);		
+		if (this.options.selectable) this.element.addClass(this.options.className + '-down');
+		return this;
+	},
+
+	onMouseUp: function(e) {
+		e.target = this;
+		this.fireEvent(Event.MOUSE_UP, e);		
+		if (this.options.selectable) this.element.removeClass(this.options.className + '-down');
+		return this;
+	}
+
+});
