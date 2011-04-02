cbfbabea7a7c597a7e41b2aa75904f61b15e24e2
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-04-02 12:36:01 -0400

    Updated the slide transition so the content of the NavigaitonBar also transition correctly.

--- Source/ViewControllerTransition/ViewControllerTransition.Slide.js
+++ Source/ViewControllerTransition/ViewControllerTransition.Slide.js
@@ -46,12 +46,16 @@ Moobile.ViewControllerTransition.Slide = new Class({
 		if (direction == 'enter') {
 			this.wrapper.addClass('transition-slide');
 			this.wrapper.addClass('transition-slide-enter');
+			this.viewControllerStack.getViewControllerAt(0).view.addClass('transition-slide-element-to-enter');
+			this.viewControllerStack.getViewControllerAt(1).view.addClass('transition-slide-element-to-leave');
 			return this;
 		}
 
 		if (direction == 'leave') {
 			this.wrapper.addClass('transition-slide');
 			this.wrapper.addClass('transition-slide-leave');
+			this.viewControllerStack.getViewControllerAt(0).view.addClass('transition-slide-element-to-leave');
+			this.viewControllerStack.getViewControllerAt(1).view.addClass('transition-slide-element-to-enter');
 			return this;
 		}
 
@@ -71,6 +75,12 @@ Moobile.ViewControllerTransition.Slide = new Class({
 			this.wrapper.removeClass('transition-slide-enter');
 			this.wrapper.removeClass('transition-slide-leave');
 			this.wrapper.removeClass('commit-transition');
+			this.viewControllerStack.getViewControllerAt(0).view
+				.removeClass('transition-slide-element-to-leave')
+				.removeClass('transition-slide-element-to-enter');
+			this.viewControllerStack.getViewControllerAt(1).view
+				.removeClass('transition-slide-element-to-leave')
+				.removeClass('transition-slide-element-to-enter');
 			this.complete();
 		}
 		return this;
