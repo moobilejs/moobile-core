dfa9da8fed4c93164895b10dd0d52f00d65fe040
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-09-05 09:20:24 -0400

    Class.intanciate will return null if the class cannot be found

--- Source/Class/Class.Instanciate.js
+++ Source/Class/Class.Instanciate.js
@@ -18,25 +18,26 @@ provides:
 */
 
 Class.extend({
-	
+
 	parse: function(name) {
 		name = name.trim();
 		name = name.split('.');
 		var func = window;
-		for (var i = 0; i < name.length; i++) func = func[name[i]];
+		for (var i = 0; i < name.length; i++) if (func[name[i]]) func = func[name[i]]; else return null;
 		return typeof func == 'function' ? func : null;
 	},
-	
+
 	instanciate: function(klass) {
 		if (typeof klass == 'string') klass = Class.parse(klass);
+		if (klass == null) return null;
 		klass.$prototyping = true;
 		var instance = new klass;
 		delete klass.$prototyping;
 		var params = Array.prototype.slice.call(arguments, 1);
 		if (instance.initialize) {
 			instance.initialize.apply(instance, params);
-		}		
-		return instance;		
+		}
+		return instance;
 	}
 
 });
