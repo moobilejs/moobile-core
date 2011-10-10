250646239355b26e585ed751de2dd337f28c35ae
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-10-10 13:56:40 -0400

    Added the possibility to group multiple roles in one declaration

--- Source/Class/Class.Mutators.Roles.js
+++ Source/Class/Class.Mutators.Roles.js
@@ -20,6 +20,23 @@ provides:
 ...
 */
 
-Class.Mutators.Roles = function(roles) {
-	this.prototype.$roles = Object.append(this.prototype.$roles || {} ,roles || {});
+(function() {
+
+var expand = function(object) {
+    for (var key in object) {
+        var val = object[key];
+        if (key.match(',')) {
+            key.split(',').each(function(k) {
+                object[k.trim()] = val;
+            });
+            delete object[key];
+        }
+    }
+    return object;
 };
+	
+Class.Mutators.Roles = function(roles) {
+	this.prototype.$roles = Object.append(this.prototype.$roles || {}, expand(roles) || {});
+};	
+	
+})();
