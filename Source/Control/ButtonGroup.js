e1f2e899340ab3815e314188ab406fdcd3ee1ca9
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-10-29 19:11:21 -0400

    Refactored the ButtonGroup class, it now extends the Entity class

--- Source/Control/ButtonGroup.js
+++ Source/Control/ButtonGroup.js
@@ -108,3 +108,16 @@ Moobile.ButtonGroup = new Class({
 	}
 
 });
+
+/**
+ * @role button-group
+ */
+Moobile.Entity.defineRole('button-group', null, function(element, options, name) {
+	
+	var instance = Class.instanciate(element.get('data-button-group') || Moobile.ButtonGroup, element, options, name);
+	if (instance instanceof Moobile.ButtonGroup) {
+		this.addChild(instance);
+	}
+	
+	return instance;	
+});
