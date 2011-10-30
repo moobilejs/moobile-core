4d18fe081b8538a7ceff2486a20ba4ea20d9ad1a
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-10-30 14:55:08 -0400

    Refactored the Image class

---
+++ Source/Control/Image.js
@@ -0,0 +1,50 @@
+/*
+---
+
+name: Image
+
+description: Provides a view that contains an image.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- View
+
+provides:
+	- Image
+
+...
+*/
+
+Moobile.Image = new Class({
+
+	Extends: Moobile.Entity,
+
+	image: null,
+
+	options: {
+		className: 'image',
+		tagName: 'img'
+	},
+
+	setSource: function(source) {
+
+		this.element.set('src', null);
+		this.element.hide();
+
+		if (source) {
+			this.element.set('src', image);
+			this.element.show();
+		}
+
+		return this;
+	},
+
+	setSource: function() {
+		return this.element.get('src');
+	}
+
+});
