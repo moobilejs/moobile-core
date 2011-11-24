123dc10b8e8436408eef3bf3e824244fc0a010e8
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-11-23 20:35:01 -0500

    Added JSDoc to ActivityIndicator and Alert

--- Source/Control/ActivityIndicator.js
+++ Source/Control/ActivityIndicator.js
@@ -25,16 +25,30 @@ Moobile.ActivityIndicator = new Class({
 
 	Extends: Moobile.Control,
 
+	/**
+	 * The class options.
+	 * @type {Object}
+	 */
 	options: {
 		className: 'activity-indicator'
 	},
 
+	/**
+	 * Start the activity indicator animation.
+	 * @return {ActivityIndicator}
+	 * @since 0.1
+	 */
 	start: function() {
 		this.addClass('activity');
 		return this;
 	},
 
-	pause: function() {
+	/**
+	 * Start the activity indicator animation.
+	 * @return {ActivityIndicator}
+	 * @since 0.1
+	 */
+	stop: function() {
 		this.removeClass('activity');
 		return this;
 	}
@@ -46,11 +60,11 @@ Moobile.ActivityIndicator = new Class({
 //------------------------------------------------------------------------------
 
 Moobile.Entity.defineRole('activity-indicator', null, function(element, name) {
-	
+
 	var instance = Class.instantiate(element.get('data-activity-indicator') || Moobile.ActivityIndicator, element, null, name);
 	if (instance instanceof Moobile.ActivityIndicator) {
 		this.addChild(instance);
 	}
-	
-	return instance;	
+
+	return instance;
 });
--- Source/Control/Alert.js
+++ Source/Control/Alert.js
@@ -24,27 +24,65 @@ Moobile.Alert = new Class({
 
 	Extends: Moobile.Overlay,
 
+	/**
+	 * The title.
+	 * @type {Entity}
+	 */
 	title: null,
-	
+
+	/**
+	 * The message.
+	 * @type {Entity}
+	 */
 	message: null,
 
-	dialog: null,	
-	
+	/**
+	 * The dialog element.
+	 * @type {Element}
+	 */
+	dialog: null,
+
+	/**
+	 * The dialog header element.
+	 * @type {Element}
+	 */
 	header: null,
-	
+
+	/**
+	 * The dialog footer element.
+	 * @type {Element}
+	 */
 	footer: null,
-	
+
+	/**
+	 * The dialog content element.
+	 * @type {Element}
+	 */
 	content: null,
-	
+
+	/**
+	 * The buttons.
+	 * @type {Array}
+	 */
 	buttons: [],
-	
+
+	/**
+	 * The class options.
+	 * @type {Object}
+	 */
 	options: {
 		className: 'alert'
 	},
-	
+
+	/**
+	 * Set the title with either a string or an entity object.
+	 * @param {Mixed} title The title as a string or an entity object.
+	 * @returh {Alert}
+	 * @since 0.1
+	 */
 	setTitle: function(title) {
 
-		if (this.title === title) 
+		if (this.title === title)
 			return this;
 
 		if (this.title instanceof Moobile.Entity) {
@@ -52,9 +90,9 @@ Moobile.Alert = new Class({
 			this.title.destroy();
 			this.title = null;
 		}
-				
+
 		this.header.empty();
-				
+
 		if (title instanceof Moobile.Entity) {
 			this.addChild(title, 'bottom', this.header);
 		} else {
@@ -65,24 +103,35 @@ Moobile.Alert = new Class({
 
 		return this;
 	},
-	
+
+	/**
+	 * Return the title entity.
+	 * @return {Entity}
+	 * @since 0.1
+	 */
 	getTitle: function() {
 		return this.title;
 	},
 
+	/**
+	 * Set the message with either a string or an entity object.
+	 * @param {Mixed} message The message as a string or an entity object.
+	 * @returh {Alert}
+	 * @since 0.1
+	 */
 	setMessage: function(message) {
-		
+
 		if (this.message === message)
 			return this;
-		
+
 		if (this.message instanceof Moobile.Entity) {
 			this.message.removeFromOwner();
 			this.message.destroy();
 			this.message = null;
 		}
-				
+
 		this.content.empty();
-				
+
 		if (message instanceof Moobile.Entity) {
 			this.addChild(message, 'bottom', this.content);
 		} else {
@@ -93,16 +142,30 @@ Moobile.Alert = new Class({
 
 		return this;
 	},
-	
+
+	/**
+	 * Return the message entity.
+	 * @return {Entity}
+	 * @since 0.1
+	 */
 	getMessage: function() {
 		return this.message;
 	},
 
+	/**
+	 * Add a button to display at the bottom of the alert.
+	 * @param {Button} button Button
+	 * @return {Alert}
+	 * @since 0.1
+	 */
 	addButton: function(button) {
 		this.addChild(button, 'bottom', this.footer);
 		return this;
 	},
-	
+
+	/**
+	 * @see Entity#didLoad
+	 */
 	didLoad: function() {
 
 		this.parent();
@@ -110,7 +173,7 @@ Moobile.Alert = new Class({
 		this.header  = new Element('div.dialog-header');
 		this.footer  = new Element('div.dialog-footer');
 		this.content = new Element('div.dialog-content');
-		
+
 		this.dialog = new Element('div.dialog');
 		this.dialog.grab(this.header);
 		this.dialog.grab(this.content);
@@ -118,71 +181,101 @@ Moobile.Alert = new Class({
 
 		this.element.grab(this.dialog);
 	},
-	
+
+	/**
+	 * @see Entity#didAddChild
+	 */
 	didAddChild: function(entity) {
-		
+
 		this.parent(entity);
-		
+
 		if (entity instanceof Moobile.Button) {
-			entity.addEvent('click', this.bound('onButtonClick'));			
+			entity.addEvent('click', this.bound('onButtonClick'));
 			entity.addEvent('mouseup', this.bound('onButtonMouseUp'));
 			entity.addEvent('mousedown', this.bound('onButtonMouseUp'));
 			this.buttons.include(entity);
-		}	
-	},
-	
-	didRemoveChild: function(entity) {
-		
-		this.parent(entity);
-		
-		if (entity instanceof Moobile.Button) {
-			entity.removeEvent('click', this.bound('onButtonClick'));
-			entity.removeEvent('mouseup', this.bound('onButtonMouseUp'));
-			entity.removeEvent('mousedown', this.bound('onButtonMouseUp'));			
-			this.button.erase(entity);
-		}		
-	},
-	
-	willShow: function() {
-		
-		this.parent();
-		
-		if (this.buttons.length == 0) {
-			
-			var button = new Moobile.Button();
-			button.setLabel('OK');
-			button.setHighlighted(true);
-			
-			this.addButton(button);			
 		}
 	},
 
+	/**
+	 * @see Entity#didRemoveChild
+	 */
+	didRemoveChild: function(entity) {
+
+		this.parent(entity);
+
+		if (entity instanceof Moobile.Button) {
+			entity.removeEvent('click', this.bound('onButtonClick'));
+			entity.removeEvent('mouseup', this.bound('onButtonMouseUp'));
+			entity.removeEvent('mousedown', this.bound('onButtonMouseUp'));
+			this.button.erase(entity);
+		}
+	},
+
+	/**
+	 * @see Entity#willShow
+	 */
+	willShow: function() {
+
+		this.parent();
+
+		if (this.buttons.length == 0) {
+
+			var button = new Moobile.Button();
+			button.setLabel('OK');
+			button.setHighlighted(true);
+
+			this.addButton(button);
+		}
+	},
+
+	/**
+	 * @see Entity#destroy
+	 */
 	destroy: function() {
-		
+
 		this.dialog = null;
 		this.header = null;
 		this.footer = null;
 		this.content = null;
-		this.buttons = null;	
+		this.buttons = null;
 		this.message = null;
 		this.title = null;
-		
-		this.parent();		
-	},	
 
+		this.parent();
+	},
+
+	/**
+	 * Button click event hand.er
+	 * @param {Event} e The event.
+	 * @since 0.1
+	 * @ignore
+	 */
 	onButtonClick: function(e) {
 
 		this.fireEvent('buttonClick', e.target);
-		
+
 		if (this.buttons.length == 1) {
 			this.hideAnimated();
 		}
 	},
-	
+
+	/**
+	 * Button mouse up event handler.
+	 * @param {Event} e The event.
+	 * @since 0.1
+	 * @ignore
+	 */
 	onButtonMouseUp: function() {
 		this.fireEvent('buttonMouseUp');
 	},
-	
+
+	/**
+	 * Button mouse down event handler.
+	 * @param {Event} e The event.
+	 * @since 0.1
+	 * @ignore
+	 */
 	onButtonMouseDown: function() {
 		this.fireEvent('buttonMouseDown');
 	}
