e18c17d05ddc923a0739c88164edbe49686ecd61
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-11-26 14:54:37 -0500

    Added JSDoc to the Types package

--- Source/Types/Array.Extras.js
+++ Source/Types/Array.Extras.js
@@ -3,7 +3,7 @@
 
 name: Array.Extras
 
-description: Provides extra methods to the Array object.
+description: Provides extra methods to the Array prototype.
 
 license: MIT-style license.
 
@@ -16,8 +16,23 @@ provides:
 ...
 */
 
-Array.implement({
+/**
+ * Provides extra methods to the Array prototype.
+ *
+ * @name Array
+ * @class Array
+ *
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @version 0.1
+ */
+Array.implement( /** @lends Array.prototype */ {
 
+	/**
+	 * Return an element based on a callback.
+	 * @param {Function} fn The search function.
+	 * @return {Mixed}
+	 * @since 0.1
+	 */
 	find: function(fn) {
 		for (var i = 0; i < this.length; i++) {
 			var found = fn.call(this, this[i]);
@@ -27,11 +42,17 @@ Array.implement({
 		}
 		return null;
 	},
-	
+
+	/**
+	 * Return an item of the array at an offset starting from the last position.
+	 * @param {Integer} offset The offset.
+	 * @return {Mixed}
+	 * @since 0.1
+	 */
 	lastItemAt: function(offset) {
 		offset = offset ? offset : 0;
 		return this[this.length - 1 - offset] ?
 			   this[this.length - 1 - offset] :
 			   null;
-	}	
+	}
 });
--- Source/Types/String.Extras.js
+++ Source/Types/String.Extras.js
@@ -3,7 +3,7 @@
 
 name: String.Extras
 
-description: Provides extra methods to the String object.
+description: Provides extra methods to the String prototype.
 
 license: MIT-style license.
 
@@ -16,8 +16,22 @@ provides:
 ...
 */
 
+/**
+ * Provides extra methods to the String prototype.
+ *
+ * @name String
+ * @class String
+ *
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @version 0.1
+ */
 String.implement({
 
+	/**
+	 * Enhance the current camelCase method.
+	 * @return {String}
+	 * @since 0.1
+	 */
 	toCamelCase: function() {
 		return this.camelCase().replace('-', '').replace(/\s\D/g, function(match){
             return match.charAt(1).toUpperCase();
