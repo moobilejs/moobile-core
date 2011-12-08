6e07cc81d34810e002d55e3fc0527d7cb0d1fbd8
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-12-08 07:39:44 -0500

    Removed comments because they are not rendered correctly

--- Source/Class/Class.Instantiate.js
+++ Source/Class/Class.Instantiate.js
@@ -16,39 +16,25 @@ provides:
 ...
 */
 
+Class.extend({
 
-/**
- * Transforms the name of a class stored in a string into an object that
- * can be instantiated.
- * @param {String} name The class name.
- * @return {Object}
- * @since 0.1
- */
-Class.parse = function(name) {
-	name = name.trim();
-	name = name.split('.');
-	var func = window;
-	for (var i = 0; i < name.length; i++) if (func[name[i]]) func = func[name[i]]; else return null;
-	return typeof func == 'function' ? func : null;
-},
+	parse: function(name) {
+		name = name.trim();
+		name = name.split('.');
+		var func = window;
+		for (var i = 0; i < name.length; i++) if (func[name[i]]) func = func[name[i]]; else return null;
+		return typeof func == 'function' ? func : null;
+	},
 
-/**
- * Creates an instance of the class given as a string or an object. Extra
- * parameters passed to this method will be transfered to the class
- * constructor.
- * @param {Object} klass The object to instantiate.
- * @return {Object}
- * @since 0.1
- */
-Class.instantiate = function(klass) {
-	if (typeof klass == 'string') klass = Class.parse(klass);
-	if (klass == null) return null;
-	klass.$prototyping = true;
-	var instance = new klass;
-	delete klass.$prototyping;
-	var params = Array.prototype.slice.call(arguments, 1);
-	if (instance.initialize) {
-		instance.initialize.apply(instance, params);
+	instantiate: function(klass) {
+		if (typeof klass == 'string') klass = Class.parse(klass);
+		if (klass == null) return null;
+		klass.$prototyping = true;
+		var instance = new klass;
+		delete klass.$prototyping;
+		var params = Array.prototype.slice.call(arguments, 1);
+		if (instance.initialize) instance.initialize.apply(instance, params);
+		return instance;
 	}
-	return instance;
-}
+
+});
