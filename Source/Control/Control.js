2ce8041fffe8b84ddd34ca8e043d7e7cfe3b9e49
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-12-08 07:37:17 -0500

    Updated JSDoc comments for JSDoc 3

--- Source/Control/Control.js
+++ Source/Control/Control.js
@@ -17,11 +17,14 @@ provides:
 */
 
 /**
- * Provides the base class for controls.
+ * @name  Control
+ * @class Provides the base class for controls.
  *
- * @name Control
- * @class Control
- * @extends Entity
+ * @classdesc
+ *
+ * This class contains the method that should be used in all controls. You
+ * should not instantiate this class, instead you should extend this class to
+ * create your own control.
  *
  * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
  * @version 0.1
@@ -31,38 +34,44 @@ Moobile.Control = new Class( /* @lends Control.prototype */ {
 	Extends: Moobile.Entity,
 
 	/**
-	 * Whether the control is disabled.
-	 * @type {Boolean}
+	 * @var    {Boolean} Whether the control is disabled.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	disabled: false,
 
 	/**
-	 * Whether the control is selected.
-	 * @type {Boolean}
+	 * @var    {Boolean} Whether the control is selected.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	selected: false,
 
 	/**
-	 * Whether the control is selectable.
-	 * @type {Boolean}
-	 */
-	selectable: true,
-
-	/**
-	 * Whether the control is highlighted.
-	 * @type {Boolean}
+	 * @var    {Boolean} Whether the control is highlighted.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	highlighted: false,
 
 	/**
-	 * Whether the control is highlightable.
-	 * @type {Boolean}
+	 * @var    {Boolean} Whether the control is selectable.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	selectable: true,
+
+	/**
+	 * @var    {Boolean} Whether the control is highlightable.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	highlightable: true,
 
 	/**
-	 * The control options.
-	 * @type {Object}
+	 * @var    {Object} The control options.
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	options: {
 		className: null,
@@ -70,106 +79,181 @@ Moobile.Control = new Class( /* @lends Control.prototype */ {
 	},
 
 	/**
-	 * Set whether the control is disabled.
-	 * @param {Boolean} disabled Whether the control is disable.
-	 * @return {Control}
-	 * @since 0.1
+	 * Sets whether the control is disabled.
+	 *
+	 * This method will add the `is-disabled` CSS class to the control's
+	 * element upon disabling. What can and cannot be done when a control is
+	 * disabled is up to the control's implementation, however, core events
+	 * will not be propagated when a control is disabled.
+	 *
+	 * @param {Boolean} value Whether the control is disabled.
+	 *
+	 * @return {Control} This control.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
-	setDisabled: function(disabled) {
-		return this._setState('disabled', disabled);
+	setDisabled: function(value) {
+		return this._setState('disabled', value);
 	},
 
 	/**
 	 * Indicate whether the control is disabled.
-	 * @return {Boolean}
-	 * @since 0.1
+	 *
+	 * What can and cannot be done when a control is disabled is up to the
+	 * control's implementation, however, core events will not be propagated when a
+	 * control is disabled.
+	 *
+	 * @return {Boolean} Whether the control is disabled.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	isDisabled: function() {
 		return this._getState('disabled');
 	},
 
 	/**
-	 * Set whether the control is selected.
-	 * @param {Boolean} selected Whether the control is selected.
-	 * @return {Control}
-	 * @since 0.1
+	 * Sets whether the control is selected.
+	 *
+	 * This method will add the `is-selected` CSS class to the control's
+	 * element upon selecting. What can and cannot be done when a control is
+	 * selected is up to the control's implementation as the selected state
+	 * is mostly used for presentation purposes.
+	 *
+	 * @param {Boolean} value Whether the control is selected.
+	 *
+	 * @return {Control} This control.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
-	setSelected: function(selected) {
-		return this.selectable ? this._setState('selected', selected) : this;
+	setSelected: function(value) {
+		return this._setState('selected', this.selectable ? value : false);
 	},
 
 	/**
-	 * Indicate whether the control is selected.
-	 * @return {Boolean}
-	 * @since 0.1
+	 * Indicates whether the control is selected.
+	 *
+	 * What can and cannot be done when a control is selected is up to the
+	 * control's implementation as the selected state is mostly used for
+	 * presentation purposes.
+	 *
+	 * @return {Boolean} Whether the control is selected.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	isSelected: function() {
 		return this._getState('selected');
 	},
 
 	/**
-	 * Set whether the control is selectable.
-	 * @param {Boolean} selectable Whether the control is selectable.
-	 * @return {Control}
-	 * @since 0.1
+	 * Sets whether the control is highlighted.
+	 *
+	 * This method will add the `is-highlighted` CSS class to the control's
+	 * element upon highlighting. What can and cannot be done when a control is
+	 * selected is up to the control's implementation as the highlighted state
+	 * is mostly used for presentation purposes.
+	 *
+	 * @param {Boolean} value Whether the control is highlighted.
+	 *
+	 * @return {Control} This control.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
-	setSelectable: function(selectable) {
-		this.selectable = selectable;
-		return this;
-	},
-
-	/**
-	 * Indicate whether the control is selectable.
-	 * @param {Boolean} selectable Whether the control is selectab.e
-	 * @return {Control}
-	 * @since 0.1
-	 */
-	isSelectable: function() {
-		return this.selectable;
-	},
-
-	/**
-	 * Set whether the control is highlighted.
-	 * @param {Boolean} highlighted Whether the control is highlighted.
-	 * @return {Control}
-	 * @since 0.1
-	 */
-	setHighlighted: function(highlighted) {
-		return this.highlightable ? this._setState('highlighted', highlighted) : this;
+	setHighlighted: function(value) {
+		return this._setState('highlighted', this.highlightable ? value : false);
 	},
 
 	/**
 	 * Indicate whether the control is highlighted.
-	 * @return {Boolean}
-	 * @since 0.1
+	 *
+	 * What can and cannot be done when a control is highlighted is up to the
+	 * control's implementation as the highlighted state is mostly used for
+	 * presentation purposes.
+	 *
+	 * @return {Boolean} Whether the control is highlighted.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	isHighlighted: function() {
 		return this._getState('highlighted');
 	},
 
 	/**
-	 * Set whether the control is highlightable.
-	 * @param {Boolean} highlightable Whether the control is highlightable.
-	 * @return {Control}
-	 * @since 0.1
+	 * Sets whether the control is selectable.
+	 *
+	 * This method will set whether this control can go from an unselected
+	 * state to a selected state. However, it will not be locked as the control
+	 * will always be able to go to the unselected state.
+	 *
+	 * @param {Boolean} value Whether the control is selectable.
+	 *
+	 * @return {Control} This control.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
-	setHighlightable: function(highlightable) {
-		this.highlightable = highlightable;
+	setSelectable: function(value) {
+		this.selectable = value;
+		return this;
 	},
 
 	/**
-	 * Indicate whether the control is highlightable.
-	 * @param {Boolean} selectable Whether the control is highlightable.
-	 * @return {Control}
-	 * @since 0.1
+	 * Indicates whether the control is selectable.
+	 *
+	 * This method will indicate whether this control can go from an unselected
+	 * state to a selected state. However, it will not be locked as the control
+	 * will always be able to go to the unselected state.
+	 *
+	 * @return {Boolean} Whether the control is selectable.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	isSelectable: function() {
+		return this.selectable;
+	},
+
+	/**
+	 * Sets whether the control is highlightable.
+	 *
+	 * This method will set whether this control can go from an unhighlighted
+	 * state to an highlighted state. However, it will not be locked as the
+	 * control will always be able to go to the unhighlighted state.
+	 *
+	 * @param {Boolean} value Whether the control is highlightable.
+	 *
+	 * @return {Control} This control.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	setHighlightable: function(value) {
+		this.highlightable = value;
+		return this;
+	},
+
+	/**
+	 * Indicates whether the control is highlightable.
+	 *
+	 * This method will indicate whether this control can go from an
+	 * unhighlighted state to an highlighted state. However, it will not be
+	 * locked as the control will always be able to go to the unhighlighted
+	 * state.
+	 *
+	 * @return {Boolean} selectable Whether the control is highlightable.
+	 *
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
 	 */
 	isHighlightable: function() {
 		return this.highlightable;
 	},
 
-	/**
-	 * @private
-	 */
 	_setState: function(state, value) {
 
 		if (this[state] == value)
@@ -181,16 +265,31 @@ Moobile.Control = new Class( /* @lends Control.prototype */ {
 		if (value)	this.element.addClass(klass);
 		else		this.element.removeClass(klass);
 
-		this.fireEvent('statechange', [state, value]);
-
 		return this;
 	},
 
-	/**
-	 * @private
-	 */
 	_getState: function(state) {
 		return this.states[state] || false;
+	},
+
+	onSwipe: function(e) {
+		if (!this.disabled) this.parent(e);
+	},
+
+	onPinch: function(e) {
+		if (!this.disabled) this.parent(e);
+	},
+
+	onClick: function(e) {
+		if (!this.disabled) this.parent(e);
+	},
+
+	onMouseUp: function(e) {
+		if (!this.disabled) this.parent(e);
+	},
+
+	onMouseDown: function(e) {
+		if (!this.disabled) this.parent(e);
 	}
 
 });
