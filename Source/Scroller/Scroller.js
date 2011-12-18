cf3aff6cb8ca6e035112bc19e73a50ed5a61e1aa
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-12-18 18:52:26 -0500

    Updated the Scroller class to use a scroller engine

--- Source/Scroller/Scroller.js
+++ Source/Scroller/Scroller.js
@@ -21,26 +21,6 @@ provides:
 ...
 */
 
-(function() {
-
-iScroll.prototype._currentSize = {x: 0, y: 0};
-
-var _checkDOMChanges = iScroll.prototype._checkDOMChanges;
-
-iScroll.prototype._checkDOMChanges = function() {
-
-	_checkDOMChanges.call(this);
-
-	var size = this.wrapper.getSize();
-	if (this._currentSize.x != size.x || this._currentSize.y != size.y) {
-		this._currentSize = size;
-		this.refresh();
-	}
-
-};
-
-})();
-
 Moobile.Scroller = new Class( /** @lends Scroller.prototype */ {
 
 	Implements: [
@@ -49,193 +29,69 @@ Moobile.Scroller = new Class( /** @lends Scroller.prototype */ {
 		Class.Binds
 	],
 
-	content: null,
-
-	wrapper: null,
-
-	scroller: null,
+	engine: null,
 
 	options: {
-		useTransform: true,
-		useTransition: true,
-		hideScrollbar: true,
-		fadeScrollbar: true,
-		checkDOMChanges: true,
-		snap: false
+		engine: 'Native'
 	},
 
-	initialize: function(wrapper, content, options) {
+	initialize: function(content, options) {
 
 		this.setOptions(options);
 
-		wrapper = document.id(wrapper);
-		content = document.id(content);
+		// TODO: Add an option to autodetect the proper engine
 
-		if (content == null) {
-			content = new Element('div')
-			content.ingest(wrapper);
-			content.inject(wrapper);
+		var engine = Moobile.Scroller.Engine[this.options.engine];
+		if (engine == undefined) {
+			throw new Error('The scroller engine ' + engine + ' does not exists');
 		}
 
-		this.content = content;
-		this.wrapper = wrapper;
-
-		this.scroller = new iScroll(this.wrapper, this.options);
-
-		this.attachEvents();
+		this.engine = new engine(content);
 
 		return this;
 	},
 
-	attachEvents: function() {
-		this.scroller.options.onScrollStart = this.bound('onScrollStart');
-		this.scroller.options.onScrollMove = this.bound('onScrollMove');
-		this.scroller.options.onScrollEnd = this.bound('onScrollEnd');
-		this.scroller.options.onRefresh = this.bound('onRefresh');
-	},
-
-	detachEvents: function() {
-		this.scroller.options.onScrollStart = null;
-		this.scroller.options.onScrollMove = null;
-		this.scroller.options.onScrollEnd = null;
-		this.scroller.options.onRefresh = null;
-	},
-
-	getCurrentPage: function() {
-		return {
-			x: this.scroller.currPageX,
-			y: this.scroller.currPageY
-		};
-	},
-
-	getAbsoluteDistance: function() {
-		return {
-			x: this.scroller.absDistX,
-			y: this.scroller.absDistY
-		};
-	},
-
-	getDistance: function() {
-		return {
-			x: this.scroller.distX,
-			y: this.scroller.distY
-		};
-	},
-
-	getDirection: function() {
-		return {
-			x: this.scroller.dirX,
-			y: this.scroller.dirY
-		};
-	},
-
-	getAbsoluteStart: function() {
-		return {
-			x: this.scroller.absStartX,
-			y: this.scroller.absStartY
-		};
-	},
-
-	getStart: function() {
-		return {
-			x: this.scroller.startX,
-			y: this.scroller.startY
-		};
-	},
-
-	getPages: function() {
-		return {
-			x: this.scroller.pagesX,
-			y: this.scroller.pagesY
-		};
-	},
-
-	getOffset: function() {
-
-		// TODO: I just realized this information might be found in iscroll
-		// directly, I'll have to fix this instead of using "fancy"
-		// regular expressions
-
-		var x = 0;
-		var y = 0;
-
-		var position = this.content.getStyle('-webkit-transform');
-		if (position) position = position.match(/translate3d\(-*(\d+)px, -*(\d+)px, -*(\d+)px\)/);
-		if (position) {
-			if (position[1]) x = -position[1];
-			if (position[2]) y = -position[2];
-		}
-
-		return {x: x, y: y};
-	},
-
-	isZoomed: function() {
-		return this.scroller.zoomed;
-	},
-
-	isMoved: function() {
-		return this.scroller.moved;
-	},
-
-	isReady: function() {
-		return this.scroller.isReady();
-	},
-
-	scrollTo: function(x, y, time, relative) {
-		(function() { this.scroller.scrollTo(x, y, time, relative); }).delay(5, this);
+	scrollTo: function(x, y, time) {
+		this.engine.scrollTo(x, y, time);
 		return this;
 	},
 
 	scrollToElement: function(element, time) {
-		(function() { this.scroller.scrollToElement(element, time); }).delay(5, this);
-		return this;
-	},
-
-	scrollToPage: function (pageX, pageY, time) {
-		(function() { this.scroller.scrollToPage(pageX, pageY, time); }).delay(5, this);
+		this.engine.scrollToElement(element, time);
 		return this;
 	},
 
 	refresh: function() {
-		this.scroller.refresh();
+		this.engine.refresh();
 		return this;
 	},
 
-	disable: function () {
-		this.scroller.disable();
-		return this;
+	getScroll: function() {
+		return this.engine.getScroll();
 	},
 
-	enable: function () {
-		this.scroller.enable();
-		this.scroller.refresh();
-		return this;
+	getContent: function() {
+		return this.engine.getContent();
 	},
 
-	stop: function() {
-		this.scroller.stop();
-		return this;
+	getWrapper: function() {
+		return this.engine.getWrapper();
 	},
 
 	destroy: function() {
-		this.scroller.destroy();
+		this.engine.destroy();
+		this.engine = null;
 		return this;
-	},
-
-	onRefresh: function() {
-		this.fireEvent('refresh');
-	},
-
-	onScrollStart: function() {
-		this.fireEvent('scrollstart');
-	},
-
-	onScrollMove: function() {
-		this.fireEvent('scrollmove');
-	},
-
-	onScrollEnd: function() {
-		this.fireEvent('scrollend');
 	}
 
 });
+
+(function() {
+
+window.addEvent('domready', function(e) {
+	document.addEvent('touchmove', function(e) {
+		if (!e.target.getParent('.scrollable')) e.preventDefault();
+	});
+});
+
+})();
