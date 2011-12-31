5fe943610787cb1422e24ae87032cbced9dad5e9
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2011-12-31 14:49:18 -0500

    Moved the box and page cover view transition to their own classes

---
+++ Source/ViewTransition/ViewTransition.Cover.Box.js
@@ -0,0 +1,113 @@
+/*
+---
+
+name: ViewTransition.Cover.Box
+
+description: Provides a view transition that covers the current view.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- ViewTransition.Cover
+
+provides:
+	- ViewTransition.Cover
+
+...
+*/
+
+/**
+ * @name  ViewTransition.Cover.Box
+ * @class Provides a view transition that covers the current view.
+ *
+ * @extends ViewTransition
+ *
+ * @author  Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @version 0.1
+ */
+Moobile.ViewTransition.Cover.Box = new Class({
+
+	Extends: Moobile.ViewTransition,
+
+	overlay: null,
+
+	viewToShowWrapper: null,
+
+	raiseAnimation: function(viewToShow, parentView) {
+		throw new Error('You cannot use this transition for the first view of a stack');
+	},
+
+	enterAnimation: function(viewToShow, viewToHide, parentView) {
+
+		var parentViewContent = parentView.getContent();
+
+		document.id(parentView).addEvent('animationend:once', function(e) {
+
+			e.stop();
+
+			parentViewContent.removeClass('transition-cover-box-enter');
+			viewToHide.removeClass('transition-view-to-hide');
+			viewToShow.removeClass('transition-view-to-show');
+
+			this.didEnter(viewToShow, viewToHide, parentView);
+
+		}.bind(this));
+
+		this.overlay = new Moobile.Overlay();
+		this.overlay.hide();
+		this.overlay.showAnimated();
+
+		viewToHide.addChild(this.overlay, 'header');
+
+		this.viewToShowWrapper = new Moobile.View();
+		this.viewToShowWrapper.addChild(viewToShow);
+		this.viewToShowWrapper.addClass('transition-cover-box-foreground-view-wrapper');
+
+		parentView.addChild(this.viewToShowWrapper);
+
+		parentViewContent.addClass('transition-cover-box-enter');
+		viewToHide.addClass('transition-cover-box-background-view');
+		viewToShow.addClass('transition-cover-box-foreground-view');
+		viewToHide.addClass('transition-view-to-hide');
+		viewToShow.addClass('transition-view-to-show');
+	},
+
+	leaveAnimation: function(viewToShow, viewToHide, parentView) {
+
+		var parentViewContent = parentView.getContent();
+
+		document.id(parentView).addEvent('animationend:once', function(e) {
+
+			e.stop();
+
+			parentViewContent.removeClass('transition-cover-box-leave');
+			viewToHide.removeClass('transition-cover-box-background-view');
+			viewToShow.removeClass('transition-cover-box-foreground-view');
+			viewToHide.removeClass('transition-view-to-hide');
+			viewToShow.removeClass('transition-view-to-show');
+
+			viewToHide.removeFromOwner();
+
+			this.didLeave(viewToShow, viewToHide, parentView);
+
+			this.viewToShowWrapper.removeFromOwner();
+			this.viewToShowWrapper.destroy();
+			this.viewToShowWrapper = null;
+
+			this.overlay.removeFromOwner();
+			this.overlay.destroy();
+			this.overlay = null;
+
+		}.bind(this));
+
+		this.overlay.hideAnimated();
+
+		parentViewContent.addClass('transition-cover-box-leave');
+		viewToHide.addClass('transition-view-to-hide');
+		viewToShow.addClass('transition-view-to-show');
+	}
+
+});
---
+++ Source/ViewTransition/ViewTransition.Cover.Page.js
@@ -0,0 +1,99 @@
+/*
+---
+
+name: ViewTransition.Cover.Page
+
+description: Provides a view transition that covers the current view.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- ViewTransition.Cover
+
+provides:
+	- ViewTransition.Cover
+
+...
+*/
+
+/**
+ * @name  ViewTransition.Cover.Page
+ * @class Provides a view transition that covers the current view.
+ *
+ * @extends ViewTransition
+ *
+ * @author  Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @version 0.1
+ */
+Moobile.ViewTransition.Cover.Page = new Class({
+
+	Extends: Moobile.ViewTransition,
+
+	overlay: null,
+
+	raiseAnimation: function(viewToShow, parentView) {
+		throw new Error('You cannot use this transition for the first view of a stack');
+	},
+
+	enterAnimation: function(viewToShow, viewToHide, parentView) {
+
+		var parentViewContent = parentView.getContent();
+
+		document.id(parentView).addEvent('animationend:once', function(e) {
+
+			e.stop();
+
+			parentViewContent.removeClass('transition-cover-page-enter');
+			viewToHide.removeClass('transition-view-to-hide');
+			viewToShow.removeClass('transition-view-to-show');
+
+			this.didEnter(viewToShow, viewToHide, parentView);
+
+		}.bind(this));
+
+		this.overlay = new Moobile.Overlay();
+		this.overlay.hide();
+		this.overlay.showAnimated();
+
+		viewToHide.addChild(this.overlay, 'header');
+
+		parentViewContent.addClass('transition-cover-page-enter');
+		viewToHide.addClass('transition-cover-page-background-view');
+		viewToShow.addClass('transition-cover-page-foreground-view');
+		viewToHide.addClass('transition-view-to-hide');
+		viewToShow.addClass('transition-view-to-show');
+	},
+
+	leaveAnimation: function(viewToShow, viewToHide, parentView) {
+
+		var parentViewContent = parentView.getContent();
+
+		document.id(parentView).addEvent('animationend:once', function(e) {
+
+			e.stop();
+
+			parentViewContent.removeClass('transition-cover-page-leave');
+			viewToHide.removeClass('transition-cover-page-background-view');
+			viewToShow.removeClass('transition-cover-page-foreground-view');
+			viewToHide.removeClass('transition-view-to-hide');
+			viewToShow.removeClass('transition-view-to-show');
+
+			this.didLeave(viewToShow, viewToHide, parentView);
+
+			this.overlay.removeFromOwner();
+			this.overlay.destroy();
+			this.overlay = null;
+
+		}.bind(this));
+
+		this.overlay.hideAnimated();
+
+		parentViewContent.addClass('transition-cover-page-leave');
+		viewToHide.addClass('transition-view-to-hide');
+		viewToShow.addClass('transition-view-to-show');
+	}
+
+});
