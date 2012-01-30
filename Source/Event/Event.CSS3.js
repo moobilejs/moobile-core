ecccea43e77ad286c5f368e1b0c15a604fb135de
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-01-30 13:48:19 -0500

    Fixed spacing

--- Source/Event/Event.CSS3.js
+++ Source/Event/Event.CSS3.js
@@ -23,25 +23,21 @@ provides:
 
 (function() {
 
-	/* vendor prefix */
+var prefix = '';
+if (Browser.safari || Browser.chrome || Browser.Platform.ios) {
+	prefix = 'webkit';
+} else if (Browser.firefox) {
+	prefix = 'Moz';
+} else if (Browser.opera) {
+	prefix = 'o';
+} else if (Browser.ie) {
+	prefix = 'ms';
+}
 
-	var prefix = '';
-	if (Browser.safari || Browser.chrome || Browser.Platform.ios) {
-		prefix = 'webkit';
-	} else if (Browser.firefox) {
-		prefix = 'Moz';
-	} else if (Browser.opera) {
-		prefix = 'o';
-	} else if (Browser.ie) {
-		prefix = 'ms';
-	}
+Element.NativeEvents[prefix + 'TransitionEnd'] = 2;
+Element.Events['transitionend'] = { base: (prefix + 'TransitionEnd') };
 
-	/* events */
-
-	Element.NativeEvents[prefix + 'TransitionEnd'] = 2;
-	Element.Events['transitionend'] = { base: (prefix + 'TransitionEnd') };
-
-	Element.NativeEvents[prefix + 'AnimationEnd'] = 2;
-	Element.Events['animationend'] = { base: (prefix + 'AnimationEnd') };
+Element.NativeEvents[prefix + 'AnimationEnd'] = 2;
+Element.Events['animationend'] = { base: (prefix + 'AnimationEnd') };
 
 })();
