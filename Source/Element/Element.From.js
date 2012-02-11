f0fd75c59a9777d531516c9852553c6c82bc136e
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-02-11 18:39:44 -0500

    Moved some methods to Element.From

---
+++ Source/Element/Element.From.js
@@ -0,0 +1,74 @@
+/*
+---
+
+name: Element.From
+
+description:
+
+license: MIT-style license.
+
+author:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+
+provides:
+	- Element.From
+	- Element.At
+
+...
+*/
+
+(function() {
+
+/**
+ * @see http://moobile.net/api/0.1/Element/Element.From#ElementFrom
+ *
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.1.0
+ */
+Element.from = function(text) {
+
+	switch (typeof text) {
+		case 'object': return document.id(text);
+		case 'string': return Elements.from(text).pop();
+	}
+
+	return null;
+};
+
+var elements = {};
+
+/**
+ * @see http://moobile.net/api/0.1/Element/Element.From#ElementAt
+ *
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.1.0
+ */
+Element.at = function(path, async, fn) {
+
+	var element = elements[path];
+	if (element) {
+		if (fn) fn(element);
+		return element;
+	}
+
+	async = async || false;
+
+	new Moobile.Request({
+		method: 'get',
+		async: async,
+		url: path
+	}).addEvent('success', function(response) {
+		element = Element.from(response);
+		if (fn) fn(element);
+	}).addEvent('failure', function(request) {
+		if (fn) fn(null);
+	}).send();
+
+	if (element) elements[path] = element;
+
+	return !async ? element : null;
+};
+
+})();
