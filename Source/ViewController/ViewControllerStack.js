45649512ac1abd5fcc425a590158c1665439ae39
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-02-18 11:33:58 -0500

    Made some members private also this class will refactor the current ViewController class

--- Source/ViewController/ViewControllerStack.js
+++ Source/ViewController/ViewControllerStack.js
@@ -20,89 +20,56 @@ provides:
 */
 
 /**
- * @name  ViewControllerStack
- * @class Provides a controller that manages a view stack.
- *
- * @classdesc
- *
- * [TODO: Description]
- * [TODO: Events]
- * [TODO: Options]
- *
- * @extends ViewController
- *
- * @author  Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
- * @version 0.1
+ * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.1
  */
-Moobile.ViewControllerStack = new Class( /** @lends ViewControllerStack.prototype */ {
+Moobile.ViewControllerStack = new Class({
 
 	Extends: Moobile.ViewController,
 
 	/**
-	 * Whether a child view is in a transition.
-	 * @type   Boolean
+	 * @private
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
-	inTransition: false,
+	_animating: false,
 
 	/**
-	 * The top view controller.
-	 * @type   ViewController
+	 * @overrides
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
-	topViewController: null,
-
 	loadView: function() {
 		this.view = new Moobile.ViewStack();
 	},
 
 	/**
-	 * Pushes a view controller into the stack.
-	 *
-	 * This method will add a new view controller at the end of the stack and
-	 * present it using a given view transition. The given transition will be
-	 * stored into the view controller so the same will be used when the
-	 * view controller will be popped.
-	 *
-	 * @param {ViewController} viewController The view controller.
-	 * @param {viewTransition} viewTransition The view transition.
-	 *
-	 * @return {ViewController} This view controller.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#pushViewController
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	pushViewController: function(viewController, viewTransition) {
 
-		if (this.inTransition)
+		if (this._animating)
 			return this;
 
-		if (this.topViewController === viewController)
+		if (this.getTopViewController() === viewController)
 			return this;
 
-		var viewControllerPushed = viewController; // ease of understanding
+		var childViewControllers = this.getChildViewControllers();
 
-		var viewControllerExists = this.childViewControllers.contains(viewControllerPushed);
-		if (viewControllerExists) {
-			this.removeChildViewController(viewControllerPushed);
-		}
+		viewController.removeFromParentViewController();
+		this.willPushViewController(viewController);
+		this.addChildViewController(viewController);
 
-		this.willPushViewController(viewControllerPushed);
+		var viewControllerPushed = viewController;
+		var viewControllerBefore = childViewControllers.lastItemAt(1);
 
-		this.addChildViewController(viewControllerPushed);
-
-		this.topViewController = viewControllerPushed;
-
-		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
-
-		var viewToShow = viewControllerPushed.view;
+		var viewToShow = viewControllerPushed.getView();
 		var viewToHide = viewControllerBefore
-					   ? viewControllerBefore.view
+					   ? viewControllerBefore.getView()
 					   : null;
-
-		this.inTransition = true;
 
 		viewTransition = viewTransition || new Moobile.ViewTransition.None();
 		viewTransition.addEvent('start:once', this.bound('onPushTransitionStart'));
@@ -111,8 +78,10 @@ Moobile.ViewControllerStack = new Class( /** @lends ViewControllerStack.prototyp
 			viewToShow,
 			viewToHide,
 			this.view,
-			this.childViewControllers.length === 1
+			childViewControllers.length === 1
 		);
+
+		this._animating = true;
 
 		viewControllerPushed.setViewTransition(viewTransition);
 
@@ -120,33 +89,61 @@ Moobile.ViewControllerStack = new Class( /** @lends ViewControllerStack.prototyp
 	},
 
 	/**
-	 * Pops the top view controller.
-	 *
-	 * This method will pop the top view controller using the same view
-	 * transition that was used to push it and destroys the popped view
-	 * controller once the transition finishes.
-	 *
-	 * @return {ViewController} This view controller.
-	 *
+	 * @protected
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
+	 */
+	onPushTransitionStart: function(e) {
+
+		var childViewControllers = this.getChildViewControllers();
+		var viewControllerPushed = childViewControllers.lastItemAt(0);
+		var viewControllerBefore = childViewControllers.lastItemAt(1);
+		if (viewControllerBefore) {
+			viewControllerBefore.viewWillLeave();
+		}
+
+		viewControllerPushed.viewWillEnter();
+	},
+
+	/**
+	 * @protected
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	onPushTransitionComplete: function(e) {
+
+		var childViewControllers = this.getChildViewControllers();
+		var viewControllerPushed = childViewControllers.lastItemAt(0);
+		var viewControllerBefore = childViewControllers.lastItemAt(1);
+		if (viewControllerBefore) {
+			viewControllerBefore.viewDidLeave();
+		}
+
+		this.didPushViewController(viewControllerPushed);
+
+		viewControllerPushed.viewDidEnter();
+
+		this._animating = false;
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#popViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
 	 */
 	popViewController: function() {
 
-		if (this.inTransition)
+		if (this._animating)
 			return this;
 
-		if (this.childViewControllers.length <= 1)
+		var childViewControllers = this.getChildViewControllers();
+		if (childViewControllers.length <= 1)
 			return this;
 
 		var viewControllerPopped = this.childViewControllers.lastItemAt(0);
 		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
 
 		this.willPopViewController(viewControllerPopped);
-
-		this.topViewController = viewControllerBefore;
-
-		this.inTransition = true;
 
 		var viewTransition = viewControllerPopped.viewTransition;
 		viewTransition.addEvent('start:once', this.bound('onPopTransitionStart'));
@@ -157,40 +154,32 @@ Moobile.ViewControllerStack = new Class( /** @lends ViewControllerStack.prototyp
 			this.view
 		);
 
+		this._animating = true;
+
 		return this;
 	},
 
 	/**
-	 * Pops view controllers until a given view controller is reached.
-	 *
-	 * This method will pop view controllers until the given view controller is
-	 * reached and destroy popped view controllers when the transition
-	 * finishes.
-	 *
-	 * @param {ViewController} viewController The view controller to pop to.
-	 *
-	 * @return {ViewController} This view controller.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#popViewControllerUntil
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	popViewControllerUntil: function(viewController) {
 
-		if (this.inTransition)
+		if (this._animating)
 			return this;
 
-		if (this.childViewControllers.length <= 1)
+		var childViewControllers = this.getChildViewControllers();
+		if (childViewControllers.length <= 1)
 			return this;
 
-		var viewControllerIndex = this.childViewControllers.indexOf(viewController);
+		var viewControllerIndex = this.getChildViewControllerIndex(viewController);
 		if (viewControllerIndex > -1) {
-			for (var i = this.childViewControllers.length - 2; i > viewControllerIndex; i--) {
-
-				var viewControllerToRemove = this.childViewControllers[i];
+			for (var i = childViewControllers.length - 2; i > viewControllerIndex; i--) {
+				var viewControllerToRemove = this.getChildViewControllerAt(i);
 				viewControllerToRemove.viewWillLeave();
 				viewControllerToRemove.viewDidLeave();
-				this.removeChildViewController(viewControllerToRemove);
-
+				viewControllerToRemove.removeFromParentViewController();
 				viewControllerToRemove.destroy();
 				viewControllerToRemove = null;
 			}
@@ -202,142 +191,145 @@ Moobile.ViewControllerStack = new Class( /** @lends ViewControllerStack.prototyp
 	},
 
 	/**
-	 * Returns the top view controller.
-	 *
-	 * @return {ViewController} The top view controller.
-	 *
+	 * @protected
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
-	getTopViewController: function() {
-		return this.topViewController;
-	},
-
-	/**
-	 * Indicates whether this view controller is currently pusing or popping
-	 * a child view controller using a transition.
-	 *
-	 * @return {Boolean} Whether this view controller is currently pushing or
-	 *                   popping a child view controller using a transition.
-	 *
-	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
-	 */
-	isInTransition: function() {
-		return this.inTransition;
-	},
-
-	/**
-	 * Tells this view controller it's about to push a view controller.
-	 *
-	 * The current implementation of this method does nothing. However it's a
-	 * good practice to call the parent as the implementation of this method
-	 * may change in the future.
-	 *
-	 * @param {ViewController} viewController The view controller.
-	 * @since 0.1
-	 */
-	willPushViewController: function(viewController) {
-
-	},
-
-	/**
-	 * Tells the view controller it pushed a view controller.
-	 *
-	 * The current implementation of this method does nothing. However it's a
-	 * good practice to call the parent as the implementation of this method
-	 * may change in the future.
-	 *
-	 * @param {ViewController} viewController The view controller.
-	 * @since 0.1
-	 */
-	didPushViewController: function(viewController) {
-
-	},
-
-	/**
-	 * Tells the view controller it's about to pop a view controller.
-	 *
-	 * The current implementation of this method does nothing. However it's a
-	 * good practice to call the parent as the implementation of this method
-	 * may change in the future.
-	 *
-	 * @param {ViewController} viewController The view controller.
-	 * @since 0.1
-	 */
-	willPopViewController: function(viewController) {
-
-	},
-
-	/**
-	 * Tells the view controller it popped a view controller.
-	 *
-	 * The current implementation of this method does nothing. However it's a
-	 * good practice to call the parent as the implementation of this method
-	 * may change in the future.
-	 *
-	 * @param {ViewController} viewController The view controller.
-	 * @since 0.1
-	 */
-	didPopViewController: function(viewController) {
-
-	},
-
-	willAddChildViewController: function(viewController) {
-		this.parent(viewController);
-		viewController.setViewControllerStack(this);
-	},
-
-	onPushTransitionStart: function(e) {
-
-		var viewControllerPushed = this.childViewControllers.lastItemAt(0);
-		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
-		if (viewControllerBefore) {
-			viewControllerBefore.viewWillLeave();
-		}
-
-		viewControllerPushed.viewWillEnter();
-	},
-
-	onPushTransitionComplete: function(e) {
-
-		var viewControllerPushed = this.childViewControllers.lastItemAt(0);
-		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
-		if (viewControllerBefore) {
-			viewControllerBefore.viewDidLeave();
-		}
-
-		this.didPushViewController(viewControllerPushed);
-
-		viewControllerPushed.viewDidEnter();
-
-		this.inTransition = false;
-	},
-
 	onPopTransitionStart: function(e) {
-
-		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
-		var viewControllerPopped = this.childViewControllers.lastItemAt(0);
-
+		var childViewControllers = this.getChildViewControllers();
+		var viewControllerBefore = childViewControllers.lastItemAt(1);
+		var viewControllerPopped = childViewControllers.lastItemAt(0);
 		viewControllerBefore.viewWillEnter();
 		viewControllerPopped.viewWillLeave();
 	},
 
+	/**
+	 * @protected
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
 	onPopTransitionComplete: function(e) {
 
-		var viewControllerPopped = this.childViewControllers.lastItemAt(0);
-		var viewControllerBefore = this.childViewControllers.lastItemAt(1);
+		var childViewControllers = this.getChildViewControllers();
+		var viewControllerPopped = childViewControllers.lastItemAt(0);
+		var viewControllerBefore = childViewControllers.lastItemAt(1);
 		viewControllerBefore.viewDidEnter();
 		viewControllerPopped.viewDidLeave();
-
-		this.removeChildViewController(viewControllerPopped);
+		viewControllerPopped.removeFromParentViewController();
 
 		this.didPopViewController(viewControllerPopped);
 
 		viewControllerPopped.destroy();
 		viewControllerPopped = null;
 
-		this.inTransition = false;
+		this._animating = false;
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#getTopViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	getTopViewController: function() {
+		return this.getChildViewControllers().getLast();
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#willPushViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willPushViewController: function(viewController) {
+
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#didPushViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	didPushViewController: function(viewController) {
+
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#willPopViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willPopViewController: function(viewController) {
+
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerStack#didPopViewController
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	didPopViewController: function(viewController) {
+
+	},
+
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willAddChildViewController: function(viewController) {
+		this.parent(viewController);
+		viewController.setViewControllerStack(this);
+	}
+
+});
+
+Class.refactor(Moobile.ViewController, {
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewController#viewControllerStack
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	viewControllerStack: null,
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewController#setViewControllerStack
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	setViewControllerStack: function(viewControllerStack) {
+		this.viewControllerStack = viewControllerStack;
+		return this;
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewController#getViewControllerStack
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	getViewControllerStack: function() {
+		return this.viewControllerStack;
+	},
+
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willAddChildViewController: function(viewController) {
+		this.previous(viewController);
+		if (viewController.getViewControllerStack() == null) {
+			viewController.setViewControllerStack(this.viewControllerStack);
+		}
+	},
+
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willRemoveChildViewController: function(viewController) {
+		this.previous(viewController);
+		viewController.setViewControllerStack(null);
 	}
 
 });
