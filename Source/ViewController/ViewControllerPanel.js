9e680187bee0c6c4b686917ef88ed3f0384b06e1
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-02-18 10:58:50 -0500

    Made some members private also this class will refactor the current ViewController class

--- Source/ViewController/ViewControllerPanel.js
+++ Source/ViewController/ViewControllerPanel.js
@@ -20,130 +20,158 @@ provides:
 */
 
 /**
- * @name  ViewControllerPanel
- * @class Provides an object used to manage a view panel.
- *
- * @classdesc
- *
- * [TODO: Description]
- * [TODO: Events]
- * [TODO: Options]
- *
- * @extends ViewController
- *
- * @author  Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
- * @version 0.1
+ * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.1
  */
-Moobile.ViewControllerPanel = new Class( /** @lends ViewControllerPanel.prototype */ {
+Moobile.ViewControllerPanel = new Class({
 
 	Extends: Moobile.ViewController,
 
 	/**
-	 * The main view controller.
-	 * @type   ViewController
+	 * @private
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
-	mainViewController: null,
+	_mainViewController: null,
 
 	/**
-	 * The side view controller.
-	 * @type   ViewController
+	 * @private
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
-	sideViewController: null,
+	_sideViewController: null,
 
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
 	loadView: function() {
 		this.view = new Moobile.ViewPanel();
 	},
 
 	/**
-	 * Sets the main view controller.
-	 *
-	 * This method will set the main view controller and add its view to the
-	 * view's main panel. There can be only one main view controller at time
-	 * meaning setting a new main view controller when there is an existing one
-	 * will destroy the existing one.
-	 *
-	 * @param {ViewController} mainViewController The main view controller.
-	 *
-	 * @return {ViewControllerPanel} This view controller panel.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#setMainViewController
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	setMainViewController: function(mainViewController) {
 
-		if (this.mainViewController) {
-			this.mainViewController.removeFromParentViewController();
-			this.mainViewController.destroy();
-			this.mainViewController = null;
+		if (this._mainViewController) {
+			this._mainViewController.destroy();
+			this._mainViewController = null;
 		}
 
-		this.mainViewController = mainViewController;
+		var view = mainViewController.getView();
+		if (view) {
+			this.view.addChildInto(view, this.view.getMainPanel());
+		}
 
-		this.addChildViewController(this.mainViewController, 'top', this.view.getMainPanel());
+		this.addChildViewController(mainViewController);
+
+		this._mainViewController = mainViewController;
 
 		return this;
 	},
 
 	/**
-	 * Returns the main view controller.
-	 *
-	 * @return {ViewController} The main view controller.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#getMainViewController
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	getMainViewController: function() {
-		return this.mainViewController;
+		return this._mainViewController;
 	},
 
 	/**
-	 * Sets the side view controller.
-	 *
-	 * This method will set the side view controller and add its view to the
-	 * view's side panel. There can be only one main view controller at time
-	 * meaning setting a new side view controller when there is an existing one
-	 * will destroy the existing one.
-	 *
-	 * @param {ViewController} sideViewController The side view controller.
-	 *
-	 * @return {ViewControllerPanel} This view controller panel.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#setSideViewController
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	setSideViewController: function(sideViewController) {
 
-		if (this.sideViewController) {
-			this.sideViewController.destroy();
-			this.sideViewController = null;
+		if (this._sideViewController) {
+			this._sideViewController.destroy();
+			this._sideViewController = null;
 		}
 
-		this.sideViewController = sideViewController;
+		var view = sideViewController.getView();
+		if (view) {
+			this.view.addChildInto(view, this.view.getSidePanel())
+		}
 
-		this.addChildViewController(this.sideViewController, 'top', this.view.getSidePanel());
+		this.addChildViewController(sideViewController);
+
+		this._sideViewController = _sideViewController;
 
 		return this;
 	},
 
 	/**
-	 * Returns the side view controller.
-	 *
-	 * @return {ViewController} The side view controller.
-	 *
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#getSideViewController
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
-	 * @since  0.1.0
+	 * @since  0.1
 	 */
 	getSideViewController: function() {
-		return this.sideViewController;
+		return this._sideViewController;
 	},
 
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
 	didAddChildViewController: function(viewController) {
 		this.parent(viewController);
 		viewController.setViewControllerPanel(this);
+	}
+
+});
+
+Class.refactor(Moobile.ViewController, {
+
+	viewControllerPanel: null,
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#setViewControllerPanel
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	setViewControllerPanel: function(viewControllerPanel) {
+		this.viewControllerPanel = viewControllerPanel;
+		return this
+	},
+
+	/**
+	 * @see    http://moobile.net/api/0.1/ViewController/ViewControllerPanel#getViewControllerPanel
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	getViewControllerPanel: function(viewControllerPanel) {
+		return this.viewControllerPanel;
+	},
+
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willAddChildViewController: function(viewController) {
+		this.previous(viewController);
+		if (viewController.getViewControllerPanel() === null) {
+			viewController.setViewControllerPanel(this.viewControllerPanel);
+		}
+	},
+
+	/**
+	 * @overrides
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1
+	 */
+	willRemoveChildViewController: function(viewController) {
+		this.previous(viewController);
+		viewController.setViewControllerPanel(null);
 	}
 
 });
