3ce5af6f0454dfe423bbae6fddd482794bdc7e70
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-04-17 19:27:25 -0400

    Added a packages that provides unprefixed uses of vendor-prefixed css properties

---
+++ Source/Element/Element.Style.Vendor.js
@@ -0,0 +1,68 @@
+/*
+---
+
+name: Element.Style.Vendor
+
+description: Automatically adds vendor prefix to styles
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- Core/Event
+	- Core/Element
+	- Core/Element.Event
+
+provides:
+	- Element.Style.Vendor
+
+...
+*/
+
+(function() {
+
+var setStyle = Element.prototype.setStyle;
+var getStyle = Element.prototype.getStyle;
+
+var prefixes = ['Khtml', 'O', 'Ms', 'Moz', 'Webkit'];
+
+var cache = {};
+
+Element.implement({
+
+	getPrefixed: function (property) {
+
+		property = property.camelCase();
+
+		if (property in this.style)
+			return property;
+
+		if (cache[property] !== undefined)
+			return cache[property];
+
+		var suffix = property.charAt(0).toUpperCase() + property.slice(1);
+
+		for (var i = 0; i < prefixes.length; i++) {
+			var prefixed = prefixes[i] + suffix;
+			if (prefixed in this.style) {
+				cache[property] = prefixed;
+				break
+			}
+		}
+
+		return cache[property];
+	},
+
+	setStyle: function (property, value) {
+		return setStyle.call(this, this.getPrefixed(property), value);
+	},
+
+	getStyle: function (property) {
+		return getStyle.call(this, this.getPrefixed(property));
+	}
+
+});
+
+})();
