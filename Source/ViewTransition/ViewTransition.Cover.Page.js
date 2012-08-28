104abe20409cf526c2615c20fb8e18688140eb83
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-08-28 18:58:54 -0400

    Fixed Page Cover transition

--- Source/ViewTransition/ViewTransition.Cover.Page.js
+++ Source/ViewTransition/ViewTransition.Cover.Page.js
@@ -22,13 +22,26 @@ provides:
 /**
  * @see    http://moobilejs.com/doc/latest/ViewTransition/ViewTransition.Cover.Page
  * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @edited 0.2.0
  * @since  0.1.0
  */
 Moobile.ViewTransition.Cover.Page = new Class({
 
 	Extends: Moobile.ViewTransition,
 
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
 	overlay: null,
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.2.0
+	 */
+	wrapper: null,
 
 	/**
 	 * @overridden
@@ -42,69 +55,77 @@ Moobile.ViewTransition.Cover.Page = new Class({
 	/**
 	 * @overridden
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @edited 0.2.0
 	 * @since  0.1.0
 	 */
 	enterAnimation: function(viewToShow, viewToHide, parentView) {
 
 		var parentElem = parentView.getContentElement();
 
-		document.id(parentView).addEvent('animationend:once', function(e) {
-
-			e.stop();
-
-			parentElem.removeClass('transition-cover-page-enter');
-			viewToHide.removeClass('transition-view-to-hide');
-			viewToShow.removeClass('transition-view-to-show');
-
-			this.didEnter(viewToShow, viewToHide, parentView);
-
-		}.bind(this));
-
 		this.overlay = new Moobile.Overlay();
+		this.overlay.addClass('transition-cover-page-overlay');
 		this.overlay.hide();
-		this.overlay.showAnimated();
 
-		viewToHide.addChildComponent(this.overlay, 'header');
+		parentView.addChildComponent(this.overlay);
 
-		parentElem.addClass('transition-cover-page-enter');
-		viewToHide.addClass('transition-cover-page-background-view');
-		viewToShow.addClass('transition-cover-page-foreground-view');
-		viewToHide.addClass('transition-view-to-hide');
-		viewToShow.addClass('transition-view-to-show');
+		this.wrapper = document.createElement('div');
+		this.wrapper.addClass('transition-cover-page-foreground-view-wrapper');
+		this.wrapper.wraps(viewToShow);
+
+		var onStart = function() {
+			parentElem.addClass('transition-cover-page-enter');
+			viewToHide.addClass('transition-cover-page-background-view');
+			viewToShow.addClass('transition-cover-page-foreground-view');
+			this.overlay.showAnimated();
+		}.bind(this);
+
+		var onEnd = function() {
+			parentElem.removeClass('transition-cover-page-enter');
+			this.didEnter(viewToShow, viewToHide, parentView);
+		}.bind(this);
+
+		var animation = new Moobile.Animation(this.wrapper);
+		animation.addEvent('start', onStart);
+		animation.addEvent('end', onEnd);
+		animation.start();
 	},
 
 	/**
 	 * @overridden
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @edited 0.2.0
 	 * @since  0.1.0
 	 */
 	leaveAnimation: function(viewToShow, viewToHide, parentView) {
 
 		var parentElem = parentView.getContentElement();
 
-		document.id(parentView).addEvent('animationend:once', function(e) {
+		var onStart = function() {
+			parentElem.addClass('transition-cover-page-leave');
+			this.overlay.hideAnimated();
+		}.bind(this);
 
-			e.stop();
+		var onEnd = function() {
 
 			parentElem.removeClass('transition-cover-page-leave');
 			viewToShow.removeClass('transition-cover-page-background-view');
 			viewToHide.removeClass('transition-cover-page-foreground-view');
-			viewToHide.removeClass('transition-view-to-hide');
-			viewToShow.removeClass('transition-view-to-show');
-
-			this.didLeave(viewToShow, viewToHide, parentView);
 
 			this.overlay.removeFromParentComponent();
 			this.overlay.destroy();
 			this.overlay = null;
 
-		}.bind(this));
+			this.didLeave(viewToShow, viewToHide, parentView);
 
-		this.overlay.hideAnimated();
+			this.wrapper.destroy();
+			this.wrapper = null;
 
-		parentElem.addClass('transition-cover-page-leave');
-		viewToHide.addClass('transition-view-to-hide');
-		viewToShow.addClass('transition-view-to-show');
+		}.bind(this);
+
+		var animation = new Moobile.Animation(this.wrapper);
+		animation.addEvent('start', onStart);
+		animation.addEvent('end', onEnd);
+		animation.start();
 	}
 
 });
