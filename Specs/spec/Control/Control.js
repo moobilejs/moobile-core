ee8530021aa23e384877a886d9acda0e2f970909
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-09-14 13:50:48 -0400

    Updated specs

--- Specs/spec/Control/Control.js
+++ Specs/spec/Control/Control.js
@@ -1,40 +1,58 @@
+
 describe('Control/Control', function() {
 
-	var control = null;
-
-	beforeEach(function() {
-		control = new Moobile.Control();
-//		spyOn(control, 'shouldAllowState');
-		spyOn(control, 'willChangeState');
-		spyOn(control, 'didChangeState');
-	});
-
-	afterEach(function() {
-		control = null;
-	});
+	// setDisabled, isDisabled
 
 	it('should set its state to disabled', function() {
-		control.setDisabled(true);
-		expect(control.isDisabled()).toEqual(true);
-//		expect(control.shouldAlloState).toHaveBeenCalledWith('disabled');
-		expect(control.willChangeState).toHaveBeenCalledWith('disabled');
-		expect(control.didChangeState).toHaveBeenCalledWith('disabled');
+		var c = new Moobile.Control();
+		spyOn(c, 'shouldAllowState').andCallThrough();
+		spyOn(c, 'willChangeState');
+		spyOn(c, 'didChangeState');
+		c.setDisabled();
+		expect(c.isDisabled()).toEqual(true);
+		c.setDisabled(true);
+		expect(c.isDisabled()).toEqual(true);
+		c.setDisabled(false);
+		expect(c.isDisabled()).toEqual(false);
+		expect(c.shouldAllowState).toHaveBeenCalledWith('disabled');
+		expect(c.willChangeState).toHaveBeenCalledWith('disabled');
+		expect(c.didChangeState).toHaveBeenCalledWith('disabled');
 	});
+
+	// setSelected, isSelected
 
 	it('should set its state to selected', function() {
-		control.setSelected(true);
-		expect(control.isSelected()).toEqual(true);
-//		expect(control.shouldAlloState).toHaveBeenCalledWith('disabled');
-		expect(control.willChangeState).toHaveBeenCalledWith('selected');
-		expect(control.didChangeState).toHaveBeenCalledWith('selected');
+		var c = new Moobile.Control();
+		spyOn(c, 'shouldAllowState').andCallThrough();
+		spyOn(c, 'willChangeState');
+		spyOn(c, 'didChangeState');
+		c.setSelected();
+		expect(c.isSelected()).toEqual(true);
+		c.setSelected(true);
+		expect(c.isSelected()).toEqual(true);
+		c.setSelected(false);
+		expect(c.isSelected()).toEqual(false);
+		expect(c.shouldAllowState).toHaveBeenCalledWith('selected');
+		expect(c.willChangeState).toHaveBeenCalledWith('selected');
+		expect(c.didChangeState).toHaveBeenCalledWith('selected');
 	});
 
+	// setHighlighted, isHighlighted
+
 	it('should set its state to highlighted', function() {
-		control.setHighlighted(true);
-		expect(control.isHighlighted()).toEqual(true);
-//		expect(control.shouldAlloState).toHaveBeenCalledWith('disabled');
-		expect(control.willChangeState).toHaveBeenCalledWith('highlighted');
-		expect(control.didChangeState).toHaveBeenCalledWith('highlighted');
+		var c = new Moobile.Control();
+		spyOn(c, 'shouldAllowState').andCallThrough();
+		spyOn(c, 'willChangeState');
+		spyOn(c, 'didChangeState');
+		c.setHighlighted();
+		expect(c.isHighlighted()).toEqual(true);
+		c.setHighlighted(true);
+		expect(c.isHighlighted()).toEqual(true);
+		c.setHighlighted(false);
+		expect(c.isHighlighted()).toEqual(false);
+		expect(c.shouldAllowState).toHaveBeenCalledWith('highlighted');
+		expect(c.willChangeState).toHaveBeenCalledWith('highlighted');
+		expect(c.didChangeState).toHaveBeenCalledWith('highlighted');
 	});
 
 });
