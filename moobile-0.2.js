e4722399ad910d9ca19b3fb802e1720ef3ecb0c6
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-11-10 20:15:32 -0500

    Added back the tab bar controls

--- moobile-0.2.js
+++ moobile-0.2.js
@@ -6936,6 +6936,478 @@ Moobile.Component.defineRole('text', null, function(element) {
 /*
 ---
 
+name: Tab
+
+description: Provides a Tab control.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- Control
+
+provides:
+	- Tab
+
+...
+*/
+
+/**
+ * @see    http://moobilejs.com/doc/latest/Control/Tab
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Tab = new Class({
+
+	Extends: Moobile.Control,
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	_label: null,
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	_image: null,
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	willBuild: function() {
+
+		this.parent();
+
+		this.addClass('tab');
+
+		var image = this.getRoleElement('image');
+		var label = this.getRoleElement('label');
+
+		if (label === null) {
+			label = document.createElement('div');
+			label.ingest(this.element);
+			label.inject(this.element);
+			label.setRole('label');
+		}
+
+		if (image === null) {
+			image = document.createElement('div');
+			image.inject(this.element, 'top');
+			image.setRole('image');
+		}
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	destroy: function() {
+		this._label = null;
+		this._image = null;
+		this.parent();
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/Tab#setLabel
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	setLabel: function(label) {
+
+		if (this._label === label)
+			return this;
+
+		label = Moobile.Text.from(label);
+
+		if (this._label) {
+			this._label.replaceWithComponent(label, true);
+		} else {
+			this.addChildComponent(label);
+		}
+
+		this._label = label;
+		this._label.addClass('tab-label');
+		this.toggleClass('tab-label-empty', this._label.isEmpty());
+
+		return this;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/Tab#getLabel
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	getLabel: function() {
+		return this._label;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/Tab#setImage
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	setImage: function(image) {
+
+		if (this._image === image)
+			return this;
+
+		image = Moobile.Image.from(image);
+
+		if (this._image) {
+			this._image.replaceWithComponent(image, true);
+		} else {
+			this.addChildComponent(image);
+		}
+
+		this._image = image;
+		this._image.addClass('tab-image');
+		this.toggleClass('tab-image-empty', this._image.isEmpty());
+
+		return this;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/Tab#getLabel
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getImage: function() {
+		return this._image;
+	}
+
+});
+
+/**
+ * @see    http://moobilejs.com/doc/latest/Control/Tab#from
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Tab.from = function(source) {
+	if (source instanceof Moobile.Tab) return source;
+	var tab = new Moobile.Tab();
+	tab.setLabel(source);
+	return tab;
+};
+
+//------------------------------------------------------------------------------
+// Roles
+//------------------------------------------------------------------------------
+
+/**
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Component.defineRole('tab', Moobile.TabBar, null, function(element) {
+	this.addTab(Moobile.Component.create(Moobile.Tab, element, 'data-tab'));
+});
+
+/**
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Component.defineRole('label', Moobile.Tab, null, function(element) {
+	this.setLabel(Moobile.Component.create(Moobile.Text, element, 'data-label'));
+});
+
+/**
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Component.defineRole('image', Moobile.Tab, null, function(element) {
+	this.setImage(Moobile.Component.create(Moobile.Image, element, 'data-image'));
+});
+
+
+
+/*
+---
+
+name: TabBar
+
+description: Provides a bar control used to navigate between views.
+
+license: MIT-style license.
+
+authors:
+	- Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+
+requires:
+	- Bar
+
+provides:
+	- TabBar
+
+...
+*/
+
+/**
+ * @see    http://moobilejs.com/doc/latest/Control/TabBar
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.TabBar = new Class({
+
+	Extends: Moobile.Bar,
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	_selectedTab: null,
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	_selectedTabIndex: -1,
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#options
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	options: {
+		selectedTabIndex: -1
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	willBuild: function() {
+		this.parent();
+		this.addClass('tab-bar');
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#setSelectedTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	setSelectedTab: function(selectedTab) {
+
+		if (this._selectedTab === selectedTab)
+			return this;
+
+		if (this._selectedTab) {
+			this._selectedTab.setSelected(false);
+			this.fireEvent('deselect', this._selectedTab);
+			this._selectedTab = null;
+		}
+
+		this._selectedTabIndex = selectedTab ? this.getChildComponentIndex(selectedTab) : -1;
+
+		if (selectedTab) {
+			this._selectedTab = selectedTab;
+			this._selectedTab.setSelected(true);
+			this.fireEvent('select', this._selectedTab);
+		}
+
+		return this;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#getSelectedTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getSelectedTab: function() {
+		return this._selectedTab;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#setSelectedTabIndex
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	setSelectedTabIndex: function(index) {
+
+		var child = null;
+		if (index >= 0) {
+			child = this.getChildComponentOfTypeAt(Moobile.Tab, index);
+		}
+
+		return this.setSelectedTab(child);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#getSelectedTabIndex
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getSelectedTabIndex: function() {
+		return this._selectedTabIndex;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#clearSelectedTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	clearSelectedTab: function() {
+		this.setSelectedTab(null);
+		return this;
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#addTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	addTab: function(tab, where) {
+		return this.addChildComponent(Moobile.Tab.from(tab), where);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#addTabAfter
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	addTabAfter: function(tab, after) {
+		return this.addChildComponentAfter(Moobile.Tab.from(tab), after);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#addTabBefore
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	addTabBefore: function(tab, before) {
+		return this.addChildComponentBefore(Moobile.Tab.from(tab), before);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#getTabs
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getTabs: function() {
+		return this.getChildComponentsOfType(Moobile.Tab);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#getTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getTab: function(name) {
+		return this.getChildComponentOfType(Moobile.Tab, name);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#getTabAt
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	getTabAt: function(index) {
+		return this.getChildComponentOfTypeAt(Moobile.Tab, index);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#removeTab
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	removeTab: function(tab, destroy) {
+		return this.removeChildComponent(tab, destroy);
+	},
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Control/TabBar#removeAllTabs
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	removeAllTabs: function(destroy) {
+		return this.removeAllChildComponentsOfType(Moobile.Tab, destroy);
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	willRemoveChildComponent: function(component) {
+		this.parent(component);
+		if (this._selectedTab === component) {
+			this.clearSelectedTab();
+		}
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	didAddChildComponent: function(child) {
+		this.parent(child);
+		if (child instanceof Moobile.Tab) {
+			child.addEvent('tap', this.bound('onTabTap'));
+		}
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	didRemoveChildComponent: function(child) {
+		this.parent(child);
+		if (child instanceof Moobile.Tab) {
+			child.removeEvent('tap', this.bound('onTabTap'));
+		}
+	},
+
+	/**
+	 * @overridden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	didChangeState: function(state) {
+		this.parent(state)
+		if (state === 'disabled' || state == null) {
+			this.getChildComponents().invoke('setDisabled', state);
+		}
+	},
+
+	/**
+	 * @hidden
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.3.0
+	 */
+	onTabTap: function(e, sender) {
+		this.setSelectedTab(sender);
+	}
+
+});
+
+//------------------------------------------------------------------------------
+// Roles
+//------------------------------------------------------------------------------
+
+/**
+ * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+ * @since  0.3.0
+ */
+Moobile.Component.defineRole('tab-bar', null, null, function(element) {
+	this.addChildComponent(Moobile.Component.create(Moobile.TabBar, element, 'data-tab-bar'));
+});
+
+
+/*
+---
+
 name: Alert
 
 description: Provides a control that displays a modal alert message.
