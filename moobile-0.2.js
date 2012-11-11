dc52c8824a9f7bd91de110fa657cc8d3100893a2
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-11-10 20:43:36 -0500

    Fixed the title and images

--- moobile-0.2.js
+++ moobile-0.2.js
@@ -10675,7 +10675,7 @@ if (!window.Moobile) window.Moobile = {};
 /**
  * @see    http://moobilejs.com/doc/latest/ViewController/ViewController
  * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
- * @edited 0.2.1
+ * @edited 0.3.0
  * @edited 0.2.0
  * @since  0.1.0
  */
@@ -11154,18 +11154,12 @@ Moobile.ViewController = new Class({
 		if (this._title === title)
 			return this;
 
-		if (typeof title === 'string') {
-			var text = title;
-			title = new Moobile.Text();
-			title.setText(text);
+		title = Moobile.Text.from(title);
+
+		if (this._title &&
+			this._title.hasParentComponent()) {
+			this._title.replaceWithComponent(title, true);
 		}
-
-		// Not totally sure about that yet
-		// var parent = this._title ? this._title.getParentComponent() : null;
-		// if (parent) {
-		// 	parent.replaceChildComponent(this._title, title);
-		// }
-
 
 		this._title = title;
 
@@ -11191,17 +11185,12 @@ Moobile.ViewController = new Class({
 		if (this._image === image)
 			return this;
 
-		if (typeof image === 'string') {
-			var source = image;
-			image = new Moobile.Image();
-			image.setSource(source);
-		}
+		image = Moobile.Image.from(image);
 
-		// Not totally sure about that yet
-		// var parent = this._image ? this._image.getParentComponent() : null;
-		// if (parent) {
-		//	parent.replaceChildComponent(this._image, image);
-		// }
+		if (this._image &&
+			this._image.hasParentComponent()) {
+			this._image.replaceWithComponent(image, true);
+		}
 
 		this._image = image;
 
