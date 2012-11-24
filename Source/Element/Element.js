035474921d45177e6bc152c4e84d974805218b42
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-11-24 14:21:28 -0500

    Removed Element.Role as it's been deprecated

--- Source/Element/Element.js
+++ Source/Element/Element.js
@@ -27,6 +27,24 @@ var adopt = Element.prototype.adopt;
 Element.implement({
 
 	/**
+	 * @see    http://moobilejs.com/doc/latest/Element/Element#setRole
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+ 	 */
+	 setRole: function(role) {
+	 	return this.set('data-role', role);
+	 },
+
+	/**
+	 * @see    http://moobilejs.com/doc/latest/Element/Element#getRole
+	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
+	 * @since  0.1.0
+	 */
+	 getRole: function(role) {
+	 	return this.get('data-role');
+	 },
+
+	/**
 	 * @see    http://moobilejs.com/doc/latest/Element/Element#ingest
 	 * @author Jean-Philippe Dery (jeanphilippe.dery@gmail.com)
 	 * @since  0.1.0
