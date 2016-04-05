d110431e5b096887230f53c79c05df8bb8ad901e
Yannick Gagnon <yannick.gagnon@lemieuxbedard.com>
2016-04-05 11:23:01 -0400

    Fix an issue where Browser.platform is now readonly, thus can't add cordova
    Signed-off-by: Yannick Gagnon <yannick.gagnon@lemieuxbedard.com>

--- src/utils/browser/index.js
+++ src/utils/browser/index.js
@@ -1,2 +1,2 @@
 "use strict"
-Browser.platform.cordova = (window.Phonegap || window.Cordova || window.cordova) && Browser.isMobile && !Browser.safari;
+// Browser.platform.cordova = (window.Phonegap || window.Cordova || window.cordova) && Browser.isMobile && !Browser.safari;
