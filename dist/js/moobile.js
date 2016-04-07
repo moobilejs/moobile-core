418a43097a737e40be5fcaf9d55a23698df160a3
Yannick Gagnon <yannick.gagnon@lemieuxbedard.com>
2016-04-07 11:37:00 -0400

    Refactor main.js to remove previously removed and added
    Signed-off-by: Yannick Gagnon <yannick.gagnon@lemieuxbedard.com>

--- dist/js/moobile.js
+++ dist/js/moobile.js
@@ -12911,97 +12911,6 @@ Browser.isMobile = !['mac', 'linux', 'win'].contains(Browser.name);
 /*
 ---
 
-name: Mouse
-
-description: Maps mouse events to their touch counterparts
-
-authors: Christoph Pojer (@cpojer)
-
-license: MIT-style license.
-
-requires: [Custom-Event/Element.defineCustomEvent, Browser.Features.Touch]
-
-provides: Mouse
-
-...
-*/
-
-if (!Browser.Features.Touch) (function(){
-
-var down = false;
-var condition = function(event, type){
-	if (type == 'touchstart') down = true;
-	else if (type == 'touchend') down = false;
-	else if (type == 'touchmove' && !down) return false;
-
-	event.targetTouches = [];
-	event.changedTouches = event.touches = [{
-		pageX: event.page.x, pageY: event.page.y,
-		clientX: event.client.x, clientY: event.client.y
-	}];
-
-	return true;
-};
-
-Element.defineCustomEvent('touchstart', {
-
-	base: 'mousedown',
-	condition: condition
-
-}).defineCustomEvent('touchmove', {
-
-	base: 'mousemove',
-	condition: condition
-
-}).defineCustomEvent('touchend', {
-
-	base: 'mouseup',
-	condition: condition
-
-});
-
-document.addEvent('mouseup', function() {
-	down = false;
-});
-
-})();
-
-},{}],64:[function(require,module,exports){
-/*
----
-
-name: Click
-
-description: Provides a replacement for click events on mobile devices
-
-authors: Christoph Pojer (@cpojer)
-
-license: MIT-style license.
-
-requires: [Touch]
-
-provides: Click
-
-...
-*/
-
-if (Browser.Features.iOSTouch) (function(){
-
-var name = 'click';
-delete Element.NativeEvents[name];
-
-Element.defineCustomEvent(name, {
-
-	base: 'touch'
-
-});
-
-})();
-
-},{}],65:[function(require,module,exports){
-/*
----
-
 name: Pinch
 
 description: Provides a custom pinch event for touch devices
@@ -13066,7 +12975,7 @@ Element.defineCustomEvent(name, {
 
 })();
 
-},{}],66:[function(require,module,exports){
+},{}],64:[function(require,module,exports){
 /*
 ---
 
@@ -13163,129 +13072,6 @@ Element.defineCustomEvent(name, {
 
 })();
 
-},{}],67:[function(require,module,exports){
-/*
----
-
-name: Touch
-
-description: Provides a custom touch event on mobile devices
-
-authors: Christoph Pojer (@cpojer)
-
-license: MIT-style license.
-
-requires: [Core/Element.Event, Custom-Event/Element.defineCustomEvent, Browser.Features.Touch]
-
-provides: Touch
-
-...
-*/
-
-(function(){
-
-var disabled;
-
-Element.defineCustomEvent('touch', {
-
-	base: 'touchend',
-
-	condition: function(event){
-		if (disabled || event.targetTouches.length !== 0) return false;
-
-		var touch = event.changedTouches[0],
-			target = document.elementFromPoint(touch.clientX, touch.clientY);
-
-		do {
-			if (target == this) return true;
-		} while (target && (target = target.parentNode));
-
-		return false;
-	},
-
-	onEnable: function(){
-		disabled = false;
-	},
-
-	onDisable: function(){
-		disabled = true;
-	}
-
-});
-
-})();
-
-},{}],68:[function(require,module,exports){
-/*
----
-
-name: Touchhold
-
-description: Provides a custom touchhold event for touch devices
-
-authors: Christoph Pojer (@cpojer)
-
-license: MIT-style license.
-
-requires: [Core/Element.Event, Custom-Event/Element.defineCustomEvent, Browser.Features.Touch]
-
-provides: Touchhold
-
-...
-*/
-
-(function(){
-
-var name = 'touchhold',
-	delayKey = name + ':delay',
-	disabled, timer;
-
-var clear = function(e){
-	clearTimeout(timer);
-};
-
-var events = {
-
-	touchstart: function(event){
-		if (event.touches.length > 1){
-			clear();
-			return;
-		}
-		
-		timer = (function(){
-			this.fireEvent(name, event);
-		}).delay(this.retrieve(delayKey) || 750, this);
-	},
-
-	touchmove: clear,
-	touchcancel: clear,
-	touchend: clear
-
-};
-
-Element.defineCustomEvent(name, {
-
-	onSetup: function(){
-		this.addEvents(events);
-	},
-
-	onTeardown: function(){
-		this.removeEvents(events);
-	},
-
-	onEnable: function(){
-		disabled = false;
-	},
-
-	onDisable: function(){
-		disabled = true;
-		clear();
-	}
-
-});
-
-})();
-
 },{}],"/src/main.js":[function(require,module,exports){
 (function (global){
 "use strict"
@@ -13310,13 +13096,9 @@ require('../vendor/mootools-custom-event/Source/Element.defineCustomEvent.js');
 
 // mootools mobile
 require('../vendor/mootools-mobile/Source/Browser/Features.Touch.js');
-require('../vendor/mootools-mobile/Source/Browser/Mobile.js')
-require('../vendor/mootools-mobile/Source/Desktop/Mouse.js')
-require('../vendor/mootools-mobile/Source/Touch/Click.js')
-require('../vendor/mootools-mobile/Source/Touch/Pinch.js')
-require('../vendor/mootools-mobile/Source/Touch/Swipe.js')
-require('../vendor/mootools-mobile/Source/Touch/Touch.js')
-require('../vendor/mootools-mobile/Source/Touch/TouchHold.js')
+require('../vendor/mootools-mobile/Source/Browser/Mobile.js');
+require('../vendor/mootools-mobile/Source/Touch/Pinch.js');
+require('../vendor/mootools-mobile/Source/Touch/Swipe.js');
 
 // mootools utilities
 // require('./utils/browser');
@@ -13409,5 +13191,5 @@ moobile.ViewTransition.Cover.Page = moobile.ViewTransition.Page;
 
 module.exports = global.Moobile = global.moobile
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"../node_modules/iscroll/src/iscroll.js":1,"../vendor/mootools-class-extras/Source/Class.Binds.js":59,"../vendor/mootools-custom-event/Source/Element.defineCustomEvent.js":60,"../vendor/mootools-mobile/Source/Browser/Features.Touch.js":61,"../vendor/mootools-mobile/Source/Browser/Mobile.js":62,"../vendor/mootools-mobile/Source/Desktop/Mouse.js":63,"../vendor/mootools-mobile/Source/Touch/Click.js":64,"../vendor/mootools-mobile/Source/Touch/Pinch.js":65,"../vendor/mootools-mobile/Source/Touch/Swipe.js":66,"../vendor/mootools-mobile/Source/Touch/Touch.js":67,"../vendor/mootools-mobile/Source/Touch/TouchHold.js":68,"./animation/animation":7,"./component/component":8,"./component/overlay":9,"./control/activity-indicator":10,"./control/bar":11,"./control/button":13,"./control/button-group":12,"./control/control":14,"./control/image":15,"./control/list":17,"./control/list-item":16,"./control/navigation-bar":18,"./control/slider":19,"./control/tab":21,"./control/tab-bar":20,"./control/text":22,"./core":23,"./dialog/alert":24,"./event/css":25,"./event/firer":26,"./event/ready":27,"./event/tap":28,"./event/touch":29,"./scroller/scroller":32,"./scroller/scroller-iscroll":30,"./scroller/scroller-native":31,"./theme/theme":33,"./utils/class":34,"./utils/element":35,"./utils/event":36,"./utils/request":37,"./utils/type/array":38,"./utils/type/string":39,"./view-controller/view-controller":42,"./view-controller/view-controller-set":40,"./view-controller/view-controller-stack":41,"./view-transition/view-transition":52,"./view-transition/view-transition-box":43,"./view-transition/view-transition-cover":44,"./view-transition/view-transition-cubic":45,"./view-transition/view-transition-drop":46,"./view-transition/view-transition-fade":47,"./view-transition/view-transition-flip":48,"./view-transition/view-transition-none":49,"./view-transition/view-transition-page":50,"./view-transition/view-transition-slide":51,"./view/scroll-view":53,"./view/view":56,"./view/view-set":54,"./view/view-stack":55,"./window/window":58,"./window/window-controller":57,"moofx/lib/frame":2}]},{},[])("/src/main.js")
+},{"../node_modules/iscroll/src/iscroll.js":1,"../vendor/mootools-class-extras/Source/Class.Binds.js":59,"../vendor/mootools-custom-event/Source/Element.defineCustomEvent.js":60,"../vendor/mootools-mobile/Source/Browser/Features.Touch.js":61,"../vendor/mootools-mobile/Source/Browser/Mobile.js":62,"../vendor/mootools-mobile/Source/Touch/Pinch.js":63,"../vendor/mootools-mobile/Source/Touch/Swipe.js":64,"./animation/animation":7,"./component/component":8,"./component/overlay":9,"./control/activity-indicator":10,"./control/bar":11,"./control/button":13,"./control/button-group":12,"./control/control":14,"./control/image":15,"./control/list":17,"./control/list-item":16,"./control/navigation-bar":18,"./control/slider":19,"./control/tab":21,"./control/tab-bar":20,"./control/text":22,"./core":23,"./dialog/alert":24,"./event/css":25,"./event/firer":26,"./event/ready":27,"./event/tap":28,"./event/touch":29,"./scroller/scroller":32,"./scroller/scroller-iscroll":30,"./scroller/scroller-native":31,"./theme/theme":33,"./utils/class":34,"./utils/element":35,"./utils/event":36,"./utils/request":37,"./utils/type/array":38,"./utils/type/string":39,"./view-controller/view-controller":42,"./view-controller/view-controller-set":40,"./view-controller/view-controller-stack":41,"./view-transition/view-transition":52,"./view-transition/view-transition-box":43,"./view-transition/view-transition-cover":44,"./view-transition/view-transition-cubic":45,"./view-transition/view-transition-drop":46,"./view-transition/view-transition-fade":47,"./view-transition/view-transition-flip":48,"./view-transition/view-transition-none":49,"./view-transition/view-transition-page":50,"./view-transition/view-transition-slide":51,"./view/scroll-view":53,"./view/view":56,"./view/view-set":54,"./view/view-stack":55,"./window/window":58,"./window/window-controller":57,"moofx/lib/frame":2}]},{},[])("/src/main.js")
 });
